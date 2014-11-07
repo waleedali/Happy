1a3d3d64947780a626725f96c8802e1d185b7cf5
efe alatan <esa281@nyu.edu>
2014-11-06 20:06:23 -0500

    Analytics page sidebar toggle, swiping also works

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('happy', ['ionic', 'happy.controllers', 'happy.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -57,20 +57,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         }
       }
     })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
+    
 
     
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/mood');
 
-});
+});//end of config
 
