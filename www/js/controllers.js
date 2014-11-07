b63c2e3890590b265941e4634b6da8b0b8d30455
efe alatan <esa281@nyu.edu>
2014-11-06 20:06:23 -0500

    Analytics page sidebar toggle, swiping also works

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('happy.controllers', [])
 
 .controller('MoodCtrl', function($scope) {
 
@@ -8,13 +8,38 @@ angular.module('starter.controllers', [])
 	    mode:'vertical',
 	    loop: true
 	  }); 
-
-	};
-	
+	};//end of function
 
 	// init the view
 	init();
-})
+})//end of Mood Controller
+
+
+.controller('AnalyticsCtrl', function($scope, $ionicModal, $timeout){
+
+	
+
+	// Create the settings modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/settings.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeSettings = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.settings = function() {
+    $scope.modal.show();
+  };
+
+})//end of Analytics Controller
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 .controller('FriendsCtrl', function($scope, Friends) {
   $scope.friends = Friends.all();
@@ -24,5 +49,11 @@ angular.module('starter.controllers', [])
   $scope.friend = Friends.get($stateParams.friendId);
 })
 
-.controller('AccountCtrl', function($scope) {
-});
+
+//Side menu js controller 
+  function ContentController($scope, $ionicSideMenuDelegate) {
+      $scope.toggleLeft = function() {
+      $ionicSideMenuDelegate.toggleLeft();
+      };//end of toggle eval
+  }//end of function
+
